c4277423e4245ca04c87ebbcb8ccc99f41ef0f17
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2025-05-06 22:16:56 +0300

    Added reading names of files with data from .env file

--- cmd/biathlon-competitions/main.go
+++ cmd/biathlon-competitions/main.go
@@ -6,15 +6,17 @@ import (
 
 	"github.com/SANEKNAYMCHIK/biathlon-competitions/internal/app/competition"
 	"github.com/SANEKNAYMCHIK/biathlon-competitions/internal/competitionsettings"
+	"github.com/joho/godotenv"
 )
 
-const configName = "config2.json"
-const eventsName = "events2"
-const outputName = "output"
-const resultName = "resulting table"
-
+// Получение имен файлов, откуда читать данные и куда выводить, из .env файла
 // Парсинг json файла с характеристиками соревнования и вызов функции Battle
 func main() {
+	godotenv.Load()
+	configName := os.Getenv("CONFIG_NAME")
+	eventsName := os.Getenv("EVENTS_NAME")
+	outputName := os.Getenv("OUTPUT_LOGS_NAME")
+	resultName := os.Getenv("RESULT_TABLE_NAME")
 	data, err := os.ReadFile(configName)
 	if err != nil {
 		panic(err)
